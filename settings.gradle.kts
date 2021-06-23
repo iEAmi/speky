c95b2c5495f4997ca14a0b6d7d58507d2b0a2712
iEAmi <EAhmad.edge@gmail.com>
2021-06-23 14:47:33 +0430

    Create new module for extension layer.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "speky"
 include("speky-core")
+include("speky-extension")
---
+++ speky-extension/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "com.github.speky"
+version = "0.3.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
