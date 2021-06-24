cbec04941472f664b523c03db8b127130e146453
iEAmi <EAhmad.edge@gmail.com>
2021-06-24 15:00:05 +0430

    Add insertOn & updateOn global functions.

--- speky-extension/src/main/kotlin/com/github/speky/extension/GlobalFunc.kt
+++ speky-extension/src/main/kotlin/com/github/speky/extension/GlobalFunc.kt
@@ -1,11 +1,25 @@
 package com.github.speky.extension
 
+import com.github.speky.core.specification.Sink
 import com.github.speky.core.specification.Source
 import com.github.speky.core.specification.Specification
+import com.github.speky.core.specification.Value
 
 /**
  * Global version of [Specification.from] for simplicity.
  */
-@JvmOverloads
 inline fun <reified T> from(alias: String = T::class.simpleName!!.lowercase()): Source.Single<T> =
   Specification.from(alias)
+
+/**
+ * Global version of [Specification.insertOn] for simplicity.
+ */
+inline fun <reified T> insertOn(vararg values: Value<T, *>): Sink.Insert<T> =
+  Specification.insertOn(*values)
+
+/**
+ * Global version of [Specification.updateOn] for simplicity.
+ */
+inline fun <reified T> updateOn(vararg values: Value<T, *>): Sink.Update<T> =
+  Specification.updateOn(*values)
+
--- speky-extension/src/test/kotlin/com/github/speky/extenstion/GlobalFuncTest.kt
+++ speky-extension/src/test/kotlin/com/github/speky/extenstion/GlobalFuncTest.kt
@@ -1,8 +1,12 @@
 package com.github.speky.extenstion
 
 import com.github.speky.core.specification.Specification
+import com.github.speky.core.specification.Value
 import com.github.speky.extension.from
+import com.github.speky.extension.insertOn
+import com.github.speky.extension.updateOn
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 
 internal class GlobalFuncTest : FunSpec({
@@ -20,5 +24,37 @@ internal class GlobalFuncTest : FunSpec({
 
     from.alias shouldBe spec.alias
     from.classRef shouldBe spec.classRef
+  }
+
+  test("insertOn should return same value as Specification.insertOn()") {
+    val insert = insertOn<Int>(
+      Value.of("name", 10),
+      Value.of("family", 20)
+    )
+
+    val spec = Specification.insertOn<Int>(
+      Value.of("name", 10),
+      Value.of("family", 20)
+    )
+
+    insert.alias shouldBe spec.alias
+    insert.classRef shouldBe spec.classRef
+    insert.values shouldContainAll spec.values
+  }
+
+  test("updateOn should return same value as Specification.updateOn()") {
+    val update = updateOn<Int>(
+      Value.of("name", 10),
+      Value.of("family", 20)
+    )
+
+    val spec = updateOn<Int>(
+      Value.of("name", 10),
+      Value.of("family", 20)
+    )
+
+    update.alias shouldBe spec.alias
+    update.classRef shouldBe spec.classRef
+    update.values shouldContainAll spec.values
   }
 })
