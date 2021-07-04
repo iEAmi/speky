acf905e9263f019848f816c28148af4eaf70df05
iEAmi <EAhmad.edge@gmail.com>
2021-07-04 13:26:34 +0430

    Add new insertOn function that accept instance and insert it to database.

--- speky-extension/src/main/kotlin/com/github/speky/extension/GlobalFunc.kt
+++ speky-extension/src/main/kotlin/com/github/speky/extension/GlobalFunc.kt
@@ -1,9 +1,14 @@
 package com.github.speky.extension
 
+import com.github.speky.core.ClassRef
+import com.github.speky.core.Lens
+import com.github.speky.core.PropertyRef
 import com.github.speky.core.specification.Sink
 import com.github.speky.core.specification.Source
 import com.github.speky.core.specification.Specification
 import com.github.speky.core.specification.Value
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.declaredMembers
 
 /**
  * Global version of [Specification.from] for simplicity.
@@ -22,3 +27,21 @@ inline fun <reified T> insertOn(vararg values: Value<T, *>): Sink.Insert<T> =
  */
 inline fun <reified T> updateOn(vararg values: Value<T, *>): Sink.Update<T> =
   Specification.updateOn(*values)
+
+/**
+ * Global version of [Specification.insertOn] for simplicity.
+ */
+inline fun <reified T> insertOn(vararg instances: T): Sink.Insert<T> {
+  val values = instances
+    .flatMap { instance ->
+      instance!!::class.declaredMembers.filterIsInstance<KProperty1<T, *>>()
+        .map { prop ->
+          Value(
+            Lens.Focus<Any?, T>(PropertyRef.of(prop.name, ClassRef.of<T>())),
+            prop.get(instance)
+          )
+        }
+    }
+
+  return Specification.insertOn(*values.toTypedArray())
+}
