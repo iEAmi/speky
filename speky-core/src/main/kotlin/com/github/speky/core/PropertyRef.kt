466e8965f8ace711f47c1fa6bf6398d42307c099
iEAmi <EAhmad.edge@gmail.com>
2021-07-04 12:19:25 +0430

    Override equals and hashCode for PropertyRef.kt

--- speky-core/src/main/kotlin/com/github/speky/core/PropertyRef.kt
+++ speky-core/src/main/kotlin/com/github/speky/core/PropertyRef.kt
@@ -37,4 +37,23 @@ data class PropertyRef<T>(
       return PropertyRef(name, ClassRef.of(), declaringClass)
     }
   }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as PropertyRef<*>
+
+    if (name != other.name) return false
+    if (declaringClassRef != other.declaringClassRef) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = name.hashCode()
+    result = 31 * result + declaringClassRef.hashCode()
+    return result
+  }
+
 }
